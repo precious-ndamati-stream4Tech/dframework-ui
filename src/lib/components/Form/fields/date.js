a239cf97fc674911cee7a746fc19c1f6fa971afc
Adarsh <adarsh.agarwal@spraxa.com>
2025-11-06 17:20:32 +0530

    Bug #68412  :- Playbook | Date format isn't same on playbook as settings screen

--- src/lib/components/Form/fields/date.js
+++ src/lib/components/Form/fields/date.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import dayjs from 'dayjs';
 import { useStateContext } from '../../useRouter/StateProvider';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 const field = ({ column, field, fieldLabel, formik, otherProps, classes, fieldConfigs, model, mode }) => {
     let isDisabled;
     const { systemDateTimeFormat, stateData } = useStateContext(); //provider
-    console.log("systemDateTimeFormat: - ",systemDateTimeFormat);
-    console.log("stateData.dateTime: - ",stateData.dateTime);
+    let dateFormat = stateData.dateTime.split(/[-/. ]/);
+    const dayPlaceholder = dateFormat.find(part => part.includes('D')) || "DD";
+	const monthPlaceholder = dateFormat.find(part => part.includes('M')) || "MMM";
+	const yearPlaceholder = dateFormat.find(part => part.includes('Y')) || "YYYY";
     if (mode !== 'copy') {
         isDisabled = fieldConfigs?.disabled;
     }
@@ -19,27 +23,36 @@ const field = ({ column, field, fieldLabel, formik, otherProps, classes, fieldCo
     }
     const showError = !!helperText;
     const props = { variant: "standard", error: showError };
-    return <DatePicker
-        {...otherProps}
-        variant="standard"
-        readOnly={column?.readOnly === true}
-        key={field}
-        fullWidth
-        format={systemDateTimeFormat(true, false, stateData.dateTime)}
-        name={field}
-        value={dayjs(formik.values[field])}
-        onChange={(value) => {
-            const adjustedDate = dayjs(value).hour(12);
-            const isoString = adjustedDate.toISOString();
-            formik.setFieldValue(field, isoString);
-        }}
-        onBlur={formik.handleBlur}
-        helperText={formik.touched[field] && formik.errors[field]}
-        disablePast={column?.disablePast}
-        disabled={isDisabled}
-        shouldDisableDate={date => shouldDisableDate ? shouldDisableDate(date, formik) : false}
-        slotProps={{ textField: { fullWidth: true, helperText, ...props } }}
-    />
+    return (
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker
+                {...otherProps}
+                variant="standard"
+                readOnly={column?.readOnly === true}
+                key={field}
+                fullWidth
+                format={systemDateTimeFormat(true, false, stateData.dateTime)}
+                name={field}
+                localeText={{
+                    fieldMonthPlaceholder: () => monthPlaceholder,
+                    fieldDayPlaceholder: () => dayPlaceholder,
+                    fieldYearPlaceholder: () => yearPlaceholder
+                }}
+                value={dayjs(formik.values[field])}
+                onChange={(value) => {
+                    const adjustedDate = dayjs(value).hour(12);
+                    const isoString = adjustedDate.toISOString();
+                    formik.setFieldValue(field, isoString);
+                }}  
+                onBlur={formik.handleBlur}
+                helperText={formik.touched[field] && formik.errors[field]}
+                disablePast={column?.disablePast}
+                disabled={isDisabled}
+                shouldDisableDate={date => shouldDisableDate ? shouldDisableDate(date, formik) : false}
+                slotProps={{ textField: { fullWidth: true, helperText, ...props } }}
+            />
+        </LocalizationProvider>
+    );
 
 }
 
